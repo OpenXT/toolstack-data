98bd7d89bc9d40b332e749148882ab2adef3f714
David Marsland <marslandd@aol.com>
2015-12-10 16:17:25 -0500

    OXT-369 Aggregate battery stats in UIVM
    This updates the UI to read and report aggregate battery statistics
    for Charge, Time To Full, and Time to Empty
    
    Signed off by: David Marsland

--- dist/script/models/batteryModel.js
+++ dist/script/models/batteryModel.js
@@ -15,6 +15,10 @@ XenClient.UI.BatteryModel = function(bat_num) {
     this.timetoempty = 0; // Seconds, 0: Unknown
     this.timetofull = 0; // Seconds, 0: Unknown
     this.percent = 0;
+    this.aggregatestate = 0; // 0: Unknown, 1: Charging, 2: Discharging, 3: Empty, 4: Fully charged, 5: Pending charge, 6: Pending discharge
+    this.aggregatetimetoempty = 0; // Seconds, 0: Unknown
+    this.aggregatetimetofull = 0; // Seconds, 0: Unknown
+    this.aggregatepercent = 0; // the total percentage of all batteries in the system
     this.adapter_state = 0;
     // Services
     var services = {
@@ -49,7 +53,7 @@ XenClient.UI.BatteryModel = function(bat_num) {
           {
               XUICache.Batteries[this.bat_index].present=present;
           }
-         
+
         }
 
       }), error);
@@ -64,12 +68,28 @@ XenClient.UI.BatteryModel = function(bat_num) {
            {
              XUICache.Batteries[this.bat_index].percent=percent;
              self.publish(XenConstants.TopicTypes.UI_BATTERIES_CHANGED);
-           } 
-         
+           }
+
         }
 
       }), error);
     }
+
+    this._updateAggregatePercent= function(interfaces, error) {
+      interfaces.xcpmd.aggregate_battery_percentage(
+      dojo.hitch(this, function(percent){
+        if (percent == 0 || !!percent) {
+          this.aggregatepercent = percent;
+           if(this.bat_index>-1)
+           {
+             XUICache.Batteries[this.bat_index].aggregatepercent=percent;
+           }
+
+        }
+
+      }), error);
+    }
+
 
    this._updateState= function(interfaces, error) {
       interfaces.xcpmd.battery_state(this.bat_num,
@@ -80,7 +100,7 @@ XenClient.UI.BatteryModel = function(bat_num) {
           {
              XUICache.Batteries[this.bat_index].state=newState;
           }
-          
+
         }
       }), error);
     }
@@ -94,7 +114,7 @@ XenClient.UI.BatteryModel = function(bat_num) {
           {
             XUICache.Batteries[this.bat_index].timetoempty=tToEmpty;
           }
-          
+
         }
       }), error);
     }
@@ -108,10 +128,54 @@ XenClient.UI.BatteryModel = function(bat_num) {
           {
               XUICache.Batteries[this.bat_index].timetofull=tToFull;
           }
-          
+
         }
       }), error);
     }
+
+    this._updateAggregateState= function(interfaces, error) {
+      interfaces.xcpmd.aggregate_battery_state(
+       dojo.hitch(this, function(newState){
+        if (newState == 0 || !!newState) {
+          this.aggregatestate = newState;
+          if(this.bat_index>-1)
+          {
+             XUICache.Batteries[this.bat_index].aggregatestate=newState;
+          }
+
+        }
+      }), error);
+    }
+
+    this._updateAggregateTimeToEmpty= function(interfaces, error) {
+      interfaces.xcpmd.aggregate_battery_time_to_empty(
+       dojo.hitch(this, function(tToEmpty){
+        if (tToEmpty == 0 || !!tToEmpty) {
+          this.aggregatetimetoempty = tToEmpty;
+          if(this.bat_index>-1)
+          {
+            XUICache.Batteries[this.bat_index].aggregatetimetoempty=tToEmpty;
+            self.publish(XenConstants.TopicTypes.UI_BATTERIES_CHANGED);
+          }
+
+        }
+      }), error);
+    }
+
+    this._updateAggregateTimeToFull= function(interfaces, error) {
+      interfaces.xcpmd.aggregate_battery_time_to_full(
+       dojo.hitch(this, function(tToFull){
+        if (tToFull == 0 || !!tToFull) {
+          this.aggregatetimetofull = tToFull;
+          if(this.bat_index>-1)
+          {
+              XUICache.Batteries[this.bat_index].aggregatetimetofull=tToFull;
+          }
+
+        }
+      }), error);
+    }
+
 
    this._updateAdapterStatus= function(interfaces, error) {
       interfaces.xcpmd.get_ac_adapter_state(
@@ -123,7 +187,7 @@ XenClient.UI.BatteryModel = function(bat_num) {
                 XUICache.Batteries[this.bat_index].adapter_state=acState;
                 self.publish(XenConstants.TopicTypes.UI_BATTERIES_CHANGED);
           }
-          
+
         }
       }), error);
     }
@@ -143,7 +207,10 @@ XenClient.UI.BatteryModel = function(bat_num) {
       this._updateTimeToFull(interfaces, error);
       this._updateTimeToEmpty(interfaces, error);
       this._updateAdapterStatus(interfaces, error);
-
+      this._updateAggregatePercent(interfaces,error);
+      this._updateAggregateTimeToFull(interfaces, error);
+      this._updateAggregateTimeToEmpty(interfaces, error);
+  
     }
 
 
@@ -189,6 +256,21 @@ XenClient.UI.BatteryModel = function(bat_num) {
     this.getState = function() {
       return this.state;
     };
+
+    this.getAggregatePercent = function() {
+        return Math.round(this.aggregatepercent);
+    };
+
+    this.getAggregateTimeToEmpty = function() {
+        return this._getTime(this.aggregatetimetoempty);
+    };
+
+    this.getAggregateTimeToFull = function() {
+        return this._getTime(this.aggregatetimetofull);
+    };
+
+
+
 };
 
 // Used to encapsulate code for checking if it's a power source the UI should be interested in
